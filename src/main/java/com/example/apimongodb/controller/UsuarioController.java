cd12e01767e5af0f354df0a495a8a1d3f7221984
Thalita Policarpo <thalita_policarpo@outlook.com>
2024-06-11 22:06:19 -0300

    atualização do CRUD

--- src/main/java/com/example/apimongodb/controller/UsuarioController.java
+++ src/main/java/com/example/apimongodb/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -15,12 +16,27 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public void save(@RequestBody Usuario usuario) {
-        usuarioService.save(usuario);
+    public Usuario save(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
     }
 
     @GetMapping
     public List<Usuario> findAll() {
         return usuarioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Usuario> findById(@PathVariable String id) {
+        return usuarioService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario update(@PathVariable String id, @RequestBody Usuario usuario) {
+        return usuarioService.update(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        usuarioService.delete(id);
     }
 }
--- src/main/java/com/example/apimongodb/service/UsuarioService.java
+++ src/main/java/com/example/apimongodb/service/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -17,10 +18,24 @@ public class UsuarioService {
         return usuarioRepository.findAll();
     }
 
+    public Optional<Usuario> findById(String id) {
+        return usuarioRepository.findById(id);
+    }
+
     public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+    public Usuario update(String id, Usuario usuario) {
+        if (usuarioRepository.existsById(id)) {
+            usuario.setId(id);
+            return usuarioRepository.save(usuario);
+        } else {
+            throw new RuntimeException("Usuário não encontrado com o ID: " + id);
+        }
+    }
 
-
+    public void delete(String id) {
+        usuarioRepository.deleteById(id);
+    }
 }
